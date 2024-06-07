94f4334a26f1370aacdfa48099dba26bdd7e912e
yunusey <ynsmrycl2007@gmail.com>
2024-06-06 23:06:11 -0400

    Add feature to toggle fullscreen

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,5 +1,13 @@
 extends Control
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_fullscreen"):
+		print(DisplayServer.window_get_mode())
+		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
 func _on_disk_method_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://DiskMethod/DiskMethod.tscn")
 
